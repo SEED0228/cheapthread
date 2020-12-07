89323facc49b47a6dee866dff483500d8c863e21
vagrant <vagrant@localhost.localdomain>
2020-12-07 21:18:57 +0000

    complete login function

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  scope module: :public do
+    devise_for :end_users, controllers: {
+      sessions: 'public/end_users/sessions',
+      registrations: 'public/end_users/registrations'
+    }
+  end
+
+  namespace :admin do
+    devise_for :admins,
+    path: '', controllers: {
+      sessions: 'admin/admins/sessions'
+    }
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
