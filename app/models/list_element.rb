883c556286e75bd53013cb132afd4ff665d6bc0b
SEED0228 <naokitane6828@gmail.com>
2021-12-05 21:16:53 +0900

    価格、カロリーのバリデーション追加

--- app/models/list_element.rb
+++ app/models/list_element.rb
@@ -3,4 +3,14 @@
 class ListElement < ApplicationRecord
   belongs_to :list
   validates :name, presence: true
+  validate :price_presence
+  validate :calorie_presence
+
+  def price_presence
+    errors.add(:base, "price can't be blank or must be 1 or more") if list.contains_price && (price || 0) <= 0
+  end
+
+  def calorie_presence
+    errors.add(:base, "calorie can't be blank or must be 1 or more") if list.contains_calorie && (calorie || 0) <= 0
+  end
 end
