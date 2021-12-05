507dbc62379599effeed18d015c20683a4ea9349
SEED0228 <naokitane6828@gmail.com>
2021-12-05 23:51:14 +0900

    編集してしまったマイグレーションを修正

--- db/migrate/20201208214637_add_calorie_to_list_element.rb
+++ db/migrate/20201208214637_add_calorie_to_list_element.rb
@@ -1,7 +1,7 @@
 class AddCalorieToListElement < ActiveRecord::Migration[6.0]
   def change
     add_column  :list_elements, :calorie, :integer
-    add_column  :lists,  :contains_calorie, :boolean, default: false
+    add_column  :lists,  :add_calorie, :boolean, default: false
     add_column  :lists,  :is_public, :boolean, default: true
   end
 end
