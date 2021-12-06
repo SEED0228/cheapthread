1466526bc2699238f5dce7acf0412dee2a643f29
SEED0228 <naokitane6828@gmail.com>
2021-12-07 02:47:12 +0900

    マイグレーションファイルの修正

--- db/migrate/20211206171353_change_data_list_id_to_list_elements.rb
+++ db/migrate/20211206171353_change_data_list_id_to_list_elements.rb
@@ -1,5 +1,5 @@
 class ChangeDataListIdToListElements < ActiveRecord::Migration[6.0]
   def change
-    change_column :list_elements, :list_id, :integer
+    change_column :list_elements, :list_id, 'integer USING CAST(list_id AS integer)'
   end
 end
