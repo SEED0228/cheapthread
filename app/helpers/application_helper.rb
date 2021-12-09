16992e6a33d05edab5e9c0b5f6da9542b159a3e1
SEED0228 <naokitane6828@gmail.com>
2021-12-10 04:01:06 +0900

    レイアウトを本家に近いものに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def price_permission(list)
-    list.list_elements.each do |list_element|
-      return false if list_element.price.nil? || list_element.price.zero?
-    end
-    true
-  end
-
-  def calorie_permission(list)
-    list.list_elements.each do |list_element|
-      return false if list_element.calorie.nil? || list_element.calorie.zero?
-    end
-    true
-  end
 end
