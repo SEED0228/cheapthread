4edde4c04ce1363a1b6072a12ce06659b2a4ac0b
SEED0228 <naokitane6828@gmail.com>
2021-12-19 00:36:39 +0900

    POSTできない不具合を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
